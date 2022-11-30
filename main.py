1ab1436eaeaa3abb59cd73bf117d089beffeceb9
homauon <56159005+homayon1992@users.noreply.github.com>
2022-11-30 03:08:36 -0800

    when paddle misses
    if the ball goes out of bounds at the screen, if yes reset the ball's

--- main.py
+++ main.py
@@ -40,4 +40,11 @@ while game_is_on:
         ball.bounce_x()
 
 
+    if ball.xcor() > 380 :
+        ball.reset_position()
+
+
+    if ball.xcor() < -380 :
+        ball.reset_position()
+
 screen.exitonclick()
